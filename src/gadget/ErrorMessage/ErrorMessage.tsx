e5fc80b745fa6bbbb7d3c7717bb72fd57adcd545
water <0529bill@gmail.com>
2022-01-29 19:54:28 +0800

    add comma

--- src/gadget/ErrorMessage/ErrorMessage.tsx
+++ src/gadget/ErrorMessage/ErrorMessage.tsx
@@ -17,10 +17,10 @@ function ErrorMessage(props: ErrorMessageState) {
   let message: Message = {
     card_num: "Card Number must be 16-digit number.",
     card_holder_first: "First name must not be empty.",
-    card_cvc: "CVC must be 3-digit number",
+    card_cvc: "CVC must be 3-digit number.",
     card_holder_second: "Last name must not be empty.",
-    expiration_year: "Year is empty",
-    expiration_month: "Month is empty",
+    expiration_year: "Year is empty.",
+    expiration_month: "Month is empty.",
   };
   return (
     <>
